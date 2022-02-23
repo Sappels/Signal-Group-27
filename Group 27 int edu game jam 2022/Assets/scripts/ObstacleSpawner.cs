5bf06b64fb0a361e02b0982b4b6f5b1035553f90
Sappels <sappels66@gmail.com>
2022-02-23 13:47:44 +0100

    Removed unnecessary local variable

--- Group 27 int edu game jam 2022/Assets/scripts/ObstacleSpawner.cs
+++ Group 27 int edu game jam 2022/Assets/scripts/ObstacleSpawner.cs
@@ -9,9 +9,6 @@ public class ObstacleSpawner : MonoBehaviour
 
     private float timeToSpawn;
     private float spawnTimer;
-    private int diceRoll = 0;
-
-
 
     private void Start()
     {
@@ -26,8 +23,7 @@ public class ObstacleSpawner : MonoBehaviour
         if (spawnTimer <= 0)
         {
             int _randomObj = Random.Range(0, obstacles.Count);
-            diceRoll = Random.Range(1, 4);
-            if (diceRoll == 1 || diceRoll == 2)
+            if (Random.value > 0.4f)
             {
                 if (Random.value > 0.9f)
                 {
@@ -38,9 +34,8 @@ public class ObstacleSpawner : MonoBehaviour
                     Instantiate(obstacles[_randomObj], transform.position, Quaternion.identity);
                 }
             }
-            diceRoll = 0;
 
-            timeToSpawn = Random.Range(3f, 5f);
+            timeToSpawn = Random.Range(2f, 4f);
             spawnTimer = timeToSpawn;
         }
     }
